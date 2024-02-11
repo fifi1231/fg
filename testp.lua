5fc32d83e50a7055f93c6e0d77ed1bea97301eaf
fifi1231 <101513126+fifi1231@users.noreply.github.com>
2024-02-11 20:38:48 +0100

    Update testp.lua

--- testp.lua
+++ testp.lua
@@ -1,19 +1,1 @@
-getgenv().config = {
-    autoFish = true,
-    placetoFish = "AdvancedFishing",
-    autoPresents = true,
-    invisWater = true,
-
-    userToMail = "",
-    autoMail = false,
-    sendHuges = false,
-    sendShards = false,
-
-    sendDiamonds = false,
-    minShards = 100,
-    minDiamonds = 1050000,
-    keepDiamonds = 50000,
-}
-wait(15)
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2c340ba7f63eb21c2e772c76d8d077be.lua"))()
 
