acc9107f6030293df0c7757ffcb66389e9f8872e
fifi1231 <101513126+fifi1231@users.noreply.github.com>
2024-02-17 14:19:14 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,13 +11,6 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   wait(1)
-   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
-
 task.wait(30) -- i hate library loading
 
 game:GetService("RunService"):Set3dRenderingEnabled(false)
