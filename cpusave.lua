c1d4cddfc487ee0c024b7572b8bab6fab83b2b12
fifi1231 <101513126+fifi1231@users.noreply.github.com>
2024-04-26 23:16:41 +0200

    Update cpusave.lua

--- cpusave.lua
+++ cpusave.lua
@@ -1,96 +1,56 @@
+local Workspace = game:GetService("Workspace")
+local Terrain = Workspace:WaitForChild("Terrain")
+Terrain.WaterReflectance = 0
+Terrain.WaterTransparency = 1
+Terrain.WaterWaveSize = 0
+Terrain.WaterWaveSpeed = 0
 
+local Lighting = game:GetService("Lighting")
+Lighting.Brightness = 0
+Lighting.GlobalShadows = false
+Lighting.FogEnd = 9e100
+Lighting.FogStart = 0
 
-	local Workspace = game:GetService("Workspace")
-	local Terrain = Workspace:WaitForChild("Terrain")
-	Terrain.WaterReflectance = 0
-	Terrain.WaterTransparency = 1
-	Terrain.WaterWaveSize = 0
-	Terrain.WaterWaveSpeed = 0
+sethiddenproperty(Lighting, "Technology", 2)
+sethiddenproperty(Terrain, "Decoration", false)
 
-	local Lighting = game:GetService("Lighting")
-	Lighting.Brightness = 0
-	Lighting.GlobalShadows = false
-	Lighting.FogEnd = 9e100
-	Lighting.FogStart = 0
+local function clearTextures(v)
+    if v:IsA("BasePart") and not v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+    elseif (v:IsA("Decal") or v:IsA("Texture")) then
+        v.Transparency = 1
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+        v.Lifetime = NumberRange.new(0)
+    elseif v:IsA("Explosion") then
+        v.BlastPressure = 1
+        v.BlastRadius = 1
+    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+        v.Enabled = false
+    elseif v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+        v.TextureID = 10385902758728957
+    elseif v:IsA("SpecialMesh")  then
+        v.TextureId = 0
+    elseif v:IsA("ShirtGraphic") then
+        v.Graphic = 1
+    elseif (v:IsA("Shirt") or v:IsA("Pants")) then
+        v[v.ClassName .. "Template"] = 1
+    elseif v.Name == "Foilage" and v:IsA("Folder") then
+        v:Destroy()
+    elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
+        task.wait()
+        v:Destroy()
+    end
+end
 
-	sethiddenproperty(Lighting, "Technology", 2)
-	sethiddenproperty(Terrain, "Decoration", false)
+game:GetService("Lighting"):ClearAllChildren()
 
-	local function clearTextures(v)
-		if v:IsA("BasePart") and not v:IsA("MeshPart") then
-			v.Material = "Plastic"
-			v.Reflectance = 0
-			v.Transparency = 1
-		elseif (v:IsA("Decal") or v:IsA("Texture")) then
-			v.Transparency = 1
-		elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-			v.Lifetime = NumberRange.new(0)
-		elseif v:IsA("Explosion") then
-			v.BlastPressure = 1
-			v.BlastRadius = 1
-		elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-			v.Enabled = false
-		elseif v:IsA("MeshPart") then
-			v.Material = "Plastic"
-			v.Reflectance = 0
-			v.TextureID = 10385902758728957
-		elseif v:IsA("SpecialMesh")  then
-			v.TextureId = 0
-		elseif v:IsA("ShirtGraphic") then
-			v.Graphic = 1
-		elseif (v:IsA("Shirt") or v:IsA("Pants")) then
-			v[v.ClassName .. "Template"] = 1
-		elseif v.Name == "Foilage" and v:IsA("Folder") then
-			v:Destroy()
-		elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
-			task.wait()
-			v:Destroy()
-		end
-	end
+for _, v in pairs(Workspace:GetDescendants()) do
+    clearTextures(v)
+end
 
-	game:GetService("Lighting"):ClearAllChildren()
-
-	for _, v in pairs(Workspace:GetDescendants()) do
-		clearTextures(v)
-	end
-
-	Workspace.DescendantAdded:Connect(function(v)
-		clearTextures(v)
-	end)
-
-	for _, v in pairs(game.Players:GetChildren()) do
-		for _, v2 in pairs(v.Character:GetDescendants()) do
-			if v2:IsA("BasePart") or v2:IsA("Decal") then
-				v2.Transparency = 1
-			end
-		end
-	end
-
-	game.Players.PlayerAdded:Connect(function(player)
-		player.CharacterAdded:Connect(function(character)
-			for _, v in pairs(character:GetDescendants()) do
-				if v:IsA("BasePart") or v:IsA("Decal") then
-					v.Transparency = 1
-				end
-			end
-		end)
-	end)
-
-	for i,v in pairs(game.Players.LocalPlayer.PlayerGui:GetChildren()) do
-		if v:IsA("ScreenGui") then
-			v.Enabled = false
-		end
-	end
-
-	for i, v in pairs(game:GetService("StarterGui"):GetChildren()) do
-		if v:IsA("ScreenGui") then
-			v.Enabled = false
-		end
-	end
-
-	for i, v in pairs(game:GetService("CoreGui"):GetChildren()) do
-		if v:IsA("ScreenGui") then
-			v.Enabled = false
-		end
-	end
-	setfpscap(30)
+Workspace.DescendantAdded:Connect(function(v)
+    clearTextures(v)
+end)
