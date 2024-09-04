a2e4650828c281d9cea2afadb0eadff7cb65535f
fifi1231 <101513126+fifi1231@users.noreply.github.com>
2024-09-04 17:04:37 +0200

    Update cpusave.lua

--- cpusave.lua
+++ cpusave.lua
@@ -1,56 +1,108 @@
-local Workspace = game:GetService("Workspace")
-local Terrain = Workspace:WaitForChild("Terrain")
-Terrain.WaterReflectance = 0
-Terrain.WaterTransparency = 1
-Terrain.WaterWaveSize = 0
-Terrain.WaterWaveSpeed = 0
+local Network = game:GetService("ReplicatedStorage"):WaitForChild("Network")
+local Library = require(game.ReplicatedStorage:WaitForChild('Library'))
+local Active = workspace:WaitForChild("__THINGS").__INSTANCE_CONTAINER:WaitForChild("Active")
+local Things = workspace:WaitForChild("__THINGS")
+local Player = game.Players.LocalPlayer
+local Character = Player.Character
+local HRP = Character:FindFirstChild("HumanoidRootPart")
 
-local Lighting = game:GetService("Lighting")
-Lighting.Brightness = 0
-Lighting.GlobalShadows = false
-Lighting.FogEnd = 9e100
-Lighting.FogStart = 0
+pcall(function()
+    local Lighting = game:GetService("Lighting")
+    local Terrain = workspace:FindFirstChildOfClass('Terrain')
+    
+    Terrain.WaterWaveSize = 0
+    Terrain.WaterWaveSpeed = 0
+    Terrain.WaterReflectance = 0
+    Terrain.WaterTransparency = 0
+    Lighting.GlobalShadows = false
+    Lighting.FogEnd = 9e9
+    
+    for _, v in pairs(game:GetDescendants()) do
+        if v:IsA("Part") or v:IsA("UnionOperation") or v:IsA("MeshPart") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+            v.Material = "Plastic"
+            v.Reflectance = 0
+        elseif v:IsA("Decal") then
+            v.Transparency = 1
+        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+            v.Lifetime = NumberRange.new(0)
+        elseif v:IsA("Explosion") then
+            v.BlastPressure = 1
+            v.BlastRadius = 1
+        end
+    end
+    
+    for _, v in pairs(Lighting:GetDescendants()) do
+        if v:IsA("BlurEffect") or v:IsA("SunRaysEffect") or v:IsA("ColorCorrectionEffect") or v:IsA("BloomEffect") or v:IsA("DepthOfFieldEffect") then
+            v.Enabled = false
+        end
+    end
+    
+    workspace.DescendantAdded:Connect(function(child)
+        if child:IsA('ForceField') or child:IsA('Sparkles') or child:IsA('Smoke') or child:IsA('Fire') then
+            game.RunService.Heartbeat:Wait()
+            child:Destroy()
+        end
+    end)
+end)
 
-sethiddenproperty(Lighting, "Technology", 2)
-sethiddenproperty(Terrain, "Decoration", false)
-
-local function clearTextures(v)
-    if v:IsA("BasePart") and not v:IsA("MeshPart") then
-        v.Material = "Plastic"
-        v.Reflectance = 0
-    elseif (v:IsA("Decal") or v:IsA("Texture")) then
-        v.Transparency = 1
-    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-        v.Lifetime = NumberRange.new(0)
-    elseif v:IsA("Explosion") then
-        v.BlastPressure = 1
-        v.BlastRadius = 1
-    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-        v.Enabled = false
-    elseif v:IsA("MeshPart") then
-        v.Material = "Plastic"
-        v.Reflectance = 0
-        v.TextureID = 10385902758728957
-    elseif v:IsA("SpecialMesh")  then
-        v.TextureId = 0
-    elseif v:IsA("ShirtGraphic") then
-        v.Graphic = 1
-    elseif (v:IsA("Shirt") or v:IsA("Pants")) then
-        v[v.ClassName .. "Template"] = 1
-    elseif v.Name == "Foilage" and v:IsA("Folder") then
-        v:Destroy()
-    elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
-        task.wait()
-        v:Destroy()
+local function moveToReplicatedStorage(name)
+    local item = workspace:FindFirstChild(name)
+    if item then
+        item.Parent = game.ReplicatedStorage
     end
 end
 
-game:GetService("Lighting"):ClearAllChildren()
-
-for _, v in pairs(Workspace:GetDescendants()) do
-    clearTextures(v)
+-- Low Processing
+local itemsToMove = {"animate", "PhantrancDGtp", "ALWAYS_RENDERING"}
+for _, name in ipairs(itemsToMove) do
+    pcall(moveToReplicatedStorage, name)
 end
 
-Workspace.DescendantAdded:Connect(function(v)
-    clearTextures(v)
+local thingsToMove = {"Sounds", "RandomEvents", "Flags", "Hoverboards", "Booths", "ExclusiveEggs", "ExclusiveEggPets", "BalloonGifts", "Sprinklers", "Eggs", "ShinyRelics"}
+for _, name in ipairs(thingsToMove) do
+    pcall(function()
+        local item = Things:FindFirstChild(name)
+        if item then
+            item.Parent = game.ReplicatedStorage
+        end
+    end)
+end
+
+pcall(function()
+    workspace.__THINGS.__INSTANCE_CONTAINER.ServerOwned.Parent = game.ReplicatedStorage
 end)
+
+local entities = {"Stats", "Chat", "Debris"}
+for _, entity in ipairs(entities) do
+    pcall(function()
+        for _, v in pairs(game[entity]:GetDescendants()) do
+            v:Destroy()
+        end
+    end)
+end
+
+workspace.Gravity = 0
+local Player = game.Players.LocalPlayer
+for i, v in Player.PlayerScripts:GetChildren() do
+    pcall(function()
+        v:Destroy()
+    end)
+end
+for i, v in workspace:GetChildren() do
+    if not (v.Name == "__THINGS" or v.Name == Player.Name)  then
+        pcall(function()
+            v:Destroy()
+        end)
+    end
+end
+for i, v in workspace.__THINGS:GetChildren() do
+    if not (v.Name == "__INSTANCE_CONTAINER" or v.Name == "Instances") then
+        pcall(function()
+            v:Destroy()
+        end)
+    end
+end
+local Player = game.Players.LocalPlayer
+for i, v in Player.PlayerGui:GetDescendants() do
+    pcall(function() v.Enabled = false end)
+end
